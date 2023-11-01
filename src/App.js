9d09cf33e3174332314f7a1cc52c4310bd928e16
pct-bchumak <bchumak1@phillips-connect.com>
2023-11-01 22:16:36 +0200

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import LoginForm from "./LoginForm";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
        Hello
-        {/*<LoginForm />*/}
     </div>
 
   );
